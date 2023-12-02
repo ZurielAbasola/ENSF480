47e5d71d6a99528ebde36e8dde745659b1acd5f2
Cose31 <115685477+Cose31@users.noreply.github.com>
2023-12-02 16:30:26 -0700

    Update SQLConnector.java

--- src/boundary/SQLConnector.java
+++ src/boundary/SQLConnector.java
@@ -13,7 +13,7 @@ import src.utility.*;
 public class SQLConnector extends Singleton{
     public final static String HOST = "localhost:3306";
     public final static String USER = "root";
-    public final static String PASS = "bio"; 
+    public final static String PASS = "Mysql1969?"; 
     public final static String DB = "Flights";
 
     private static final String DB_URL_PREFIX = "jdbc:mysql://";
